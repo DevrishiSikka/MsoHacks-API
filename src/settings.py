a82073bc6c46e2f6024f6cb67a1637200332b7e3
Devrishi Sikka <devrishisikka@gmail.com>
2022-09-04 15:39:04 +0530

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
-mongo_username = 'polestar'
-mongo_password = '44Rx$!!&P7wkityH^2'
-account_sid = 'ACc6ddf81479b870351f6582972bc99f74'
-auth_token = 'c324b47feddb14145394e653dc503acb'
+mongo_username = 'test'
+mongo_password = 'test'
+account_sid = 'test'
+auth_token = 'test'
